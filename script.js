4093d09f9948741559349b720f82d4ba95200fd6
lampsi19 <p.ana19@escola.pr.gov.br>
2025-03-16 15:44:52 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const aumentaFonteBotao=document.getElementById('aumentar-fonte');
+    const diminuirFonteBotao=document.getElementById('diminuir-fonte');
+    const alternaContraste=document.getElemnetById('alterna-contraste'); 
+
+    let tamanhoAtualFonte=1;
+
+    aumentaFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte += 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+
+    diminuirFonteBotao.addEventListener('click', function(){
+        tamanhoAtualFonte -= 0.1;
+        document.body.style.fontSize = `${tamanhoAtualFonte}rem`;
+    });
+    const botaoDeAcessibilidade=document.getElementById('botao-acessibilidade');
+    const opcoesDeAcessibilidade=document.getElementById('opcoes-acessibilidade');
+    botaoDeAcessibilidade.addEventListener('click', function(){
+        botaoDeAcessibilidade.classList.toggle('rotacao-botao');
+        opcoesDeAcessibilidade.classList.toggle('apresenta-lista');
+    })
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+        })
+});
